6d4b8e675c06c070a5b80590d7ac386fe2d25cdc
Sven Finke <sven.finke@mail.com>
2021-06-23 20:47:35 +0200

    start to migrate from self written controller arch to commander

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
+import { Command } from "commander";
 import { GameController } from "./controller/gameController";
 import { ControlerManager } from "./controllerManager/controllerManager";
-import { CliRequest } from "./types/cliRequest";
 import { GamestateType } from "./types/gamestateType";
 
-let request = new CliRequest(process.argv);
-let controllerManager = new ControlerManager();
+let program = new Command();
+program
+    .option('-d, --debug', 'output extra debugging')
+    .option('-m, --memory', 'use in-memory gamestate');
+
+program.parse(process.argv);
 let gamestate = GamestateType.getGamestate();
 gamestate.load();
-
-let gameController = new GameController()
-controllerManager.registerController('game', gameController);
-controllerManager.registerController('index', gameController);
-controllerManager.render(request);
 
 gamestate.persist();
